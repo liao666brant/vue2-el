e8a0d83cc6ee1c5f76287b828651c1edf3e53234
liao666brant <liao_brant@163.com>
2021-10-01 23:16:54 +0800

    安装相关依赖

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引用Element-UI框架
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 引用axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
